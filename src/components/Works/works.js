60a8cf0f7beebf977fd92b099c8b55d74c68919e
ChenWMichael <chenwm@seas.upenn.edu>
2023-12-28 21:29:41 -0800

    Changed name color in intro. Updated pop up height. Added complete descriptions for projects.

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -49,7 +49,15 @@ const Works = () => {
     {
       name: "Portfolio Website",
       imgSrc: Portfolio_Stamp,
-      description: "TBD",
+      description:
+        "Over a productive span of two days, I developed my first React-based portfolio " +
+        "website, a testament to my growing skills in modern web technologies. This interactive " +
+        "platform showcases my projects through clickable images, each leading to a detailed " +
+        "popup complete with image carousels and comprehensive descriptions. I integrated " +
+        "Email.js for direct communication and added social media links to offer a complete " +
+        "and engaging user experience. As an initial foray into React, this website not only " +
+        "demonstrates my technical capabilities but also my enthusiasm for exploring and " +
+        "mastering new technologies in the realm of web development.",
       images: [
         Portfolio_Stamp,
         portfolio_img1,
@@ -62,19 +70,58 @@ const Works = () => {
     {
       name: "COVID-19 Data Analysis",
       imgSrc: Covid_Stamp,
-      description: "TBD",
+      description:
+        "In this project, I developed a Java-based data analysis application that focuses " +
+        "on parsing and analyzing diverse datasets, including COVID-19 statistics, property " +
+        "values, and population data. The core of the project involved intricate parsing of " +
+        "CSV and JSON files to transform them into meaningful insights. I employed advanced " +
+        "data structures and integrated key design patterns like Singleton and Strategy to " +
+        "ensure a robust and scalable application architecture. Emphasizing the practical " +
+        "application of data analysis, the application features calculations of total " +
+        "vaccinations per capita from the COVID-19 dataset and average market values of " +
+        "properties. The project was structured using N-tier architecture to enhance " +
+        "maintainability and scalability, facilitating efficient data management and processing. " +
+        "Furthermore, I implemented comprehensive error handling mechanisms to manage data " +
+        "inconsistencies, ensuring the reliability and integrity of the outputs. A key focus " +
+        "was placed on efficient data management and memory usage, including techniques like " +
+        "memoization, which are critical for handling large datasets effectively.",
       images: [Covid_Stamp],
     },
     {
       name: "LC4 Reverse Assembler",
       imgSrc: LC4_Stamp,
-      description: "TBD",
+      description:
+        "In the LC4 Reverse Assembler and Memory Management Tool project, I developed a " +
+        "C program that successfully reverse assembles .obj files from PennSim into LC4 " +
+        "assembly language. The project involved complex parsing of binary .obj files and " +
+        "meticulous reconstruction of assembly code. I efficiently managed a dynamic linked " +
+        "list in C, mimicking the LC4's program and data memories, closely replicating PennSim’s " +
+        "loader functionality. Key to this project was the use of advanced bitwise operations " +
+        "for interpreting binary instructions, translating them into readable LC4 assembly " +
+        "language, thus deepening the understanding of machine code. Rigorous error handling, " +
+        "combined with Valgrind for memory leak detection, ensured tool reliability and stability " +
+        "under various conditions. Additionally, I utilized GDB for debugging, enhancing code " +
+        "reliability and performance. The focus on effective memory allocation and deallocation " +
+        "was pivotal in overcoming memory management challenges, ensuring the tool's integrity " +
+        "and performance.",
       images: [LC4_Stamp],
     },
     {
       name: "Battleship",
       imgSrc: Battleship_Stamp,
-      description: "TBD",
+      description:
+        "In the 'Battleship - A Java-Based Game Implementation' project, developed as part " +
+        "of my software development course, I recreated the classic game of Battleship with a " +
+        "twist of single-player against the computer. Over the course of this project, I honed " +
+        "my skills in Java, focusing on intricate data parsing from .obj files to lay out the " +
+        "game environment, and implementing efficient data structures like dynamic linked lists " +
+        "for managing game data. The game's core included advanced concepts such as bitwise " +
+        "operations for interpreting game moves, robust error handling, and effective memory " +
+        "management using tools like Valgrind to ensure stability. I designed an interactive " +
+        "console-based interface that allows strategic gameplay, challenging the player against " +
+        "a strategically programmed AI opponent. This endeavor not only deepened my understanding " +
+        "of object-oriented programming and software testing but also emphasized the importance " +
+        "of user-centric design in software development.",
       images: [Battleship_Stamp],
     },
   ];
